6d0cf59b1ad271fa113c2e5e60402c18f8fda8a2
Victor <cozachenko.5@gmail.com>
2023-11-28 19:20:18 +0300

    modified:   Main.java 	modified:   a.xlsx

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
@@ -29,25 +30,43 @@ public class Main {
     }
 
     private static void processCells(Row row, FormulaEvaluator formulaEvaluator, int startColumn, int endColumn) {
+        int[] columnWidths = {50, 20, 30};  // Пример: каждый столбец имеет свою ширину
+    
         for (int colIndex = startColumn; colIndex <= endColumn; colIndex++) {
             Cell cell = row.getCell(colIndex, Row.MissingCellPolicy.CREATE_NULL_AS_BLANK);
             String cellValue = getCellValue(cell, formulaEvaluator);
-            System.out.print(cellValue + "\t\t");
+    
+            // Добавление табуляции перед каждой ячейкой, кроме первой
+            if (colIndex > startColumn) {
+                System.out.print("\t");
+            }
+    
+            // Форматирование строки с заданной шириной столбца
+            System.out.print(String.format("%-" + columnWidths[colIndex - startColumn] + "s", cellValue));
         }
+        System.out.println();  // Новая строка после обработки каждой строки
     }
-
-    private static String getCellValue(Cell cell, FormulaEvaluator formulaEvaluator) {
-        switch (cell.getCellTypeEnum()) {
-            case NUMERIC:
+    
+    
+private static String getCellValue(Cell cell, FormulaEvaluator formulaEvaluator) {
+    switch (cell.getCellTypeEnum()) {
+        case NUMERIC:
+            if (DateUtil.isCellDateFormatted(cell)) {
+                // Если ячейка содержит дату, возвращаем ее в формате строки
+                return new SimpleDateFormat("yyyy-MM-dd").format(cell.getDateCellValue());
+            } else {
+                // Возвращаем числовое значение ячейки
                 return String.valueOf(cell.getNumericCellValue());
-            case STRING:
-                return cell.getStringCellValue();
-            default:
-                return cell.getCellTypeEnum() == CellType.FORMULA
-                        ? evaluateFormulaCell(cell, formulaEvaluator)
-                        : cell.getRichStringCellValue().getString();
-        }
+            }
+        case STRING:
+            // Возвращаем строковое значение ячейки
+            return cell.getStringCellValue();
+        default:
+            return cell.getCellTypeEnum() == CellType.FORMULA
+                    ? evaluateFormulaCell(cell, formulaEvaluator)
+                    : cell.getRichStringCellValue().getString();
     }
+}
 
     private static String evaluateFormulaCell(Cell cell, FormulaEvaluator formulaEvaluator) {
         // Вычисление значения формулы в ячейке
